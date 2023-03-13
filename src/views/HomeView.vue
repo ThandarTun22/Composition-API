e2902037441a41b86e7df5a535bf07b24a530a38
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-03-13 10:41:02 +0630

    lifecycle hooks in composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-if="showPosts">
+      <PostsList :posts="posts"></PostsList>
+    </div>
+    <button @click="showPosts=!showPosts">Toggle post lists</button>
+    <button @click="posts.pop()">Delete</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostsList from '../components/PostsList'
+import { ref } from 'vue'
+
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  components: { PostsList },
+  setup(){
+    let showPosts = ref(true);
+    let posts = ref([
+      {title : "post title 1", 
+       body : "  Lorem ipsum dolor, sit amet consectetur adipisicing elit.Harum ad earum libero veniam itaque, tenetur dolorum dolores, non nostrum inventore quod quae error at ipsam omnis illum velit alias consequuntur." , 
+       id:1},
+      {title : "post title 2", body : "lorem ipsum", id:2}
+    ])
+    return {posts, showPosts};
   }
 }
 </script>
