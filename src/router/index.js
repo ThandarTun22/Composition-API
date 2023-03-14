9049af63c5e0cc549f21c4ac8490a37eb6f160f7
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-03-14 15:17:58 +0630

    create post frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DetailView from "../views/DetailView.vue"
+import CreatePost from "../views/CreatePost.vue"
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: "detail",
     component: DetailView,
     props: true
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: CreatePost,
   }
 ]
 
