af102cefc21401e0ade27e6900287c7006c266a7
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-03-20 11:24:36 +0630

    fetch our single post document

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,20 +1,14 @@
 import { ref } from "vue";
+import {db} from "../firebase/config"
 
 let getPost = (id)=> {
     let post = ref(null);
     let error = ref("");
     let load = async()=>{
        try{
-        // await new Promise((resolve, reject)=>{
-        //     // resolve();
-        //     setTimeout(resolve,2000)
-        // })
-        let response =  await fetch("http://localhost:3000/posts"+ "/" + id);
-        if(response.status === 404){
-            throw new Error("not found that exact url");
-        }
-        let data = await response.json();
-        post.value = data;   //index post's null
+            let doc = await db.collection("posts").doc(id).get()
+            // console.log(doc.data())
+            post.value = {id:doc.id, ...doc.data()}
        }catch(err){
             error.value = err.message;
        }
