bb122786c15c7a42abc79c4c4ae42b16e59560cb
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-03-20 14:01:16 +0630

    using timestamps and order by created

--- src/views/CreatePost.vue
+++ src/views/CreatePost.vue
@@ -20,7 +20,7 @@
 <script>
 import { ref } from 'vue'
 import {useRouter} from 'vue-router'
-import {db} from "../firebase/config"
+import {db, timestamp} from "../firebase/config"
 export default {
     setup(){
         let router = useRouter()  // this.$router
@@ -39,7 +39,8 @@ export default {
             let newPost = {
                     title: title.value,
                     body: body.value,  //""
-                    tags: tags.value  //[]
+                    tags: tags.value,  //[]
+                    created_at: timestamp()
                 }
                let res =  await db.collection("posts").add(newPost)
               //  console.log(res)
