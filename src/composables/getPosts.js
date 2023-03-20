09eaa22fd1fd41c9faf061402fc9ff9782392204
ThandarTun22 <thandartun.tt2022@gmail.com>
2023-03-20 10:06:37 +0630

    fetch our firebase posts collection ducuments

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,20 +1,18 @@
 import { ref } from 'vue'
+import {db} from "../firebase/config"
 
 let getPosts = ()=>{
     let posts = ref([]);
     let error = ref("");
     let load = async()=>{
       try{
-      //   await new Promise((resolve, reject)=>{
-      //     // resolve();
-      //     setTimeout(resolve,2000)
-      // })
-        let response = await fetch("http://localhost:3000/posts")
-        if(response.status === 404){
-          throw new Error("not found url");
-        }
-       let datas = await response.json()
-       posts.value = datas
+        let res=await db.collection("posts").get()
+        // console.log(res.docs)
+        posts.value = res.docs.map((doc)=>{
+            // console.log(doc.data())
+            console.log(doc.id)
+            return{id:doc.id,...doc.data()}
+        })
       }catch(err){
           // console.log(error.message)
           error.value = err.message
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,5 @@
 import firebase from "firebase/app";
 import "firebase/firestore"
-
 const firebaseConfig = {
     apiKey: "AIzaSyAdMAr4HFdQ7zHBKfHM5wVLvtdu8wSfQ44",
     authDomain: "vue-block-system-39b19.firebaseapp.com",
@@ -9,10 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "144389887050",
     appId: "1:144389887050:web:568489ed33d1da270802b3"
   };
-  //init firebase
-  firebase.initializeApp(firebaseConfig)
 
-  // database setup
-  let db = firebase.firestore()
+// init firebase
+firebase.initializeApp(firebaseConfig)
 
-  export {db};
+// database setup
+let db=firebase.firestore();
+
+export {db};
